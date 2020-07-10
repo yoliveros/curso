962b18af8694ad8785866b8926292adcf33d7367
Yoelvis Oliveros <yoelvisoliveros@gmail.com>
2020-07-10 10:06:22 -0400

    first commit

---
+++ build/precache-manifest.443b6e13427822e4ac4d95db1ab1966c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d8f98be9631e4808973d10241fed8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d561a9705454677c1a27",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b634b605d0f385e3fa85",
+    "url": "/static/js/2.d1b577b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d1b577b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d561a9705454677c1a27",
+    "url": "/static/js/main.13c26bdb.chunk.js"
+  },
+  {
+    "revision": "3cd99eff9d1a7e437571",
+    "url": "/static/js/runtime-main.203f4b3d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
